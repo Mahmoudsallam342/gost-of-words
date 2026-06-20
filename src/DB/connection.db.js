957ee72acf8e651e56ddcf84d56d72cc976c5835
Mahmoudsallam342 <msallam342@gmail.com>
2026-06-20 12:15:18 +0300

    initialize project structure with Express and MongoDB integration and create Error handling file

---
+++ src/DB/connection.db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+import { DB_URL } from "../../config/config.service.js";
+
+export const connectDB = async () => {
+  try {
+    const result = await mongoose.connect(DB_URL);
+    console.log("DB connected 🚀");
+  } catch (error) {
+    console.log("DB failed to connect ❌");
+  }
+};
