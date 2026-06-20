c64312fe0253a600777bdb885e61bf33784d731e
Mahmoudsallam342 <msallam342@gmail.com>
2026-06-20 18:40:06 +0300

    add user model and enums

--- src/DB/connection.db.js
+++ src/DB/connection.db.js
@@ -4,6 +4,8 @@ import { DB_URL } from "../../config/config.service.js";
 export const connectDB = async () => {
   try {
     const result = await mongoose.connect(DB_URL);
+    await UserModel.syncIndexes();
+
     console.log("DB connected 🚀");
   } catch (error) {
     console.log("DB failed to connect ❌");
