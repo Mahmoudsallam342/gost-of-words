e19381a56d2048048418ceca5d1c03cda9b6e2c0
Mahmoudsallam342 <msallam342@gmail.com>
2026-07-19 15:01:41 +0300

    add Joi validation for user signup and login; implement global error handling middleware

--- src/common/utils/response/error.response.js
+++ src/common/utils/response/error.response.js
@@ -15,7 +15,23 @@ import { NODE_ENV } from "../../../../config/config.service.js";
 //     stack: NODE_ENV == "development" ? error.stack : undefined,
 //   });
 // };
+export const globalErrorHandling = (error, req, res, next) => {
+  const status = error.cause?.status ?? 500;
+  const mood = NODE_ENV == "production";
+  const defaultErrorMessage = "something went wrong Server error";
+  const displayErrorMessage = error.message || defaultErrorMessage;
 
+  return res.status(status).json({
+    status,
+    errorMessage: mood
+      ? status === 500
+        ? defaultErrorMessage
+        : displayErrorMessage
+      : displayErrorMessage,
+    extra: error?.cause?.extra || undefined,
+    stack: mood ? undefined : error.stack,
+  });
+};
 //!general_customized_error
 // export const ErrorResponse = ({
 //   message = "Error",
--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -6,14 +6,22 @@ import {
   signUpWithGmail,
 } from "./auth.service.js";
 import { successResponse } from "../../common/utils/response/index.js";
+import * as validators from "./auth.validation.js";
+import { validation } from "../../middleware/index.js";
 const router = Router();
-router.post("/signup", async (req, res, next) => {
-  const account = await signup(req.body);
-  return successResponse({ res, status: 201, data: { account } });
-});
-router.post("/login", async (req, res, next) => {
-  const account = await login(req.body, `${req.protocol}://${req.host}`);
-  return successResponse({ res, data: { account } });
+
+router.post(
+  "/signup",
+  validation(validators.signup),
+  async (req, res, next) => {
+    const account = await signup(req.body);
+    return successResponse({ res, status: 201, data: { account } });
+  },
+);
+
+router.post("/login", validation(validators.login), async (req, res, next) => {
+  const credentials = await login(req.body, `${req.protocol}://${req.host}`);
+  return successResponse({ res, data: { credentials } });
 });
 router.post("/signup/gmail", async (req, res, next) => {
   console.log(req.body);
